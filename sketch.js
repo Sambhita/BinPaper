7699271b74285f2eebd55c563a49a035a9395ab9
Sambhita <77112921+Sambhita@users.noreply.github.com>
2021-02-08 20:59:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var engine,world;
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(100,600,10);
+	ground = new Ground(600,height,1200,20)
+	lineL = new Bin(550,620,20,100);
+	lineR = new Bin(670,620,20,100);
+	lineB = new Bin(610,660,100,20);
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  Engine.update(engine);
+
+  ground.display();
+  paper.display();
+
+  lineL.display();
+  lineR.display();
+  lineB.display();
+
+  
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85})
+	}
+}
